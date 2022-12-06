1808676043d849636d79d3c789e10fc54f16ae12
Sanyamj0605 <86023063+Sanyamj0605@users.noreply.github.com>
2022-12-06 17:17:08 +0530

    push baby

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+import bz2
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=3a16da99b9773e318613f2811d9fe79b&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w300/" + data['poster_path']
+def recommend(movie):
+    movie_index = movies[movies['title']==movie].index[0]
+    distances = simi[movie_index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True, key = lambda x:x[1])[1:6]
+    recommend_movies=[]
+    posters=[]
+    for i in movies_list:
+        movie_id=movies.iloc[i[0]].movie_id
+        recommend_movies.append(movies.iloc[i[0]].title)
+        posters.append(fetch_poster(movie_id))
+    return recommend_movies,posters
+
+movies_dict = pickle.load(open('movies.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+simi = pickle.load(open('simi.pkl','rb'))
+
+st.title("Movie Recommendation Engine")
+selected_movie_name = st.selectbox('How would you like to be connected?',movies['title'].values)
+
+if st.button('Recommend'):
+    names,posters=recommend(selected_movie_name)
+    col1, col2, col3 ,col4, col5= st.columns(5)
+
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
